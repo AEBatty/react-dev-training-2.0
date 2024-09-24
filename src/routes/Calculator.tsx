2537426c124de7489fe0766b6579da47bf403db7
u64xpf5 <battyalberto@johndeere.com>
2024-09-23 19:17:13 -0600

    feat

--- src/routes/Calculator.tsx
+++ src/routes/Calculator.tsx
@@ -1,39 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Button } from "semantic-ui-react";
 
 export const Calculator = () => {
 
+  const[result, setResult] = useState<string>("0");
+
+  const auxResult = 0;
+
+  const solveResult = (value: string) => {
+
+
+    if(value === "C"){
+      setResult(() => {return "0"});
+    }else{
+      setResult((prevResult) => {return prevResult === ("0") ? value : (prevResult + value)});
+    }
+
+
+  }
+
   return (
     <>
 
       <div>
-        <input type="text" value="0" />
+        <input type="text" value={result} />
       </div>
       <div>
-        <Button>7</Button>
-        <Button>8</Button>
-        <Button>9</Button>
-        <Button>+</Button>
+        <Button onClick={() => {solveResult("7")}}>7</Button>
+        <Button onClick={() => {solveResult("8")}}>8</Button>
+        <Button onClick={() => {solveResult("9")}}>9</Button>
+        <Button onClick={() => {solveResult("+")}}>+</Button>
       </div>
       <div>
-        <Button>4</Button>
-        <Button>5</Button>
-        <Button>6</Button>
-        <Button>-</Button>
+        <Button onClick={() => {solveResult("4")}}>4</Button>
+        <Button onClick={() => {solveResult("5")}}>5</Button>
+        <Button onClick={() => {solveResult("6")}}>6</Button>
+        <Button onClick={() => {solveResult("-")}}>-</Button>
       </div>
       <div>
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-        <Button>*</Button>
+        <Button onClick={() => {solveResult("1")}}>1</Button>
+        <Button onClick={() => {solveResult("2")}}>2</Button>
+        <Button onClick={() => {solveResult("3")}}>3</Button>
+        <Button onClick={() => {solveResult("*")}}>*</Button>
       </div>
       <div>
-        <Button>0</Button>
-        <Button>.</Button>
-        <Button>=</Button>
-        <Button>/</Button>
+        <Button onClick={() => {solveResult("0")}}>0</Button>
+        <Button onClick={() => {solveResult(".")}}>.</Button>
+        <Button onClick={() => {solveResult("=")}}>=</Button>
+        <Button onClick={() => {solveResult("/")}}>/</Button>
       </div>
+        <Button onClick={() => {solveResult("C")}}>C</Button>
+
 
     </>
   );
--- src/routes/DigitalClock.tsx
+++ src/routes/DigitalClock.tsx
@@ -1,12 +1,31 @@
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export const DigitalClock = () => {
+
+  const[clock, setClock] = useState(new Date().toLocaleTimeString());
+
+
+  const updateClock = () => {
+    useEffect(() => {
+      const id = setInterval(() => {
+        setClock(new Date().toLocaleTimeString())
+      }, 1000);
+      console.log("init:" + id)
+      return () => {
+        clearInterval(id);      
+        console.log("des:" + id)
+      }
+    }, []);
+  }
+
+  updateClock();
+  
 
   return (
     <div>
       <h1>Digital Clock</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
+      <h2>{clock}</h2>
     </div>
   );
 };
